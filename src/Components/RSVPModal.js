5c99cb9cc9cb875813e52c7585b6793e467173de
Amulya Parab <amulyaparab101@gmail.com>
2023-07-13 22:27:28 +0530

    handled edge cases

--- src/Components/RSVPModal.js
+++ src/Components/RSVPModal.js
@@ -5,7 +5,14 @@ export const RSVPModal = ({ eventId }) => {
   const findEvent = state.filteredData.find((event) => event.id === eventId);
   return (
     <div className="overlay">
-      <div className="rsvp-form">
+      <form
+        className="rsvp-form"
+        onSubmit={(event) => {
+          event.preventDefault();
+          setShowRSVP(false);
+          dispatch({ type: "RSVP", payload: eventId });
+        }}
+      >
         <i
           class="fa-solid fa-circle-xmark"
           onClick={() => setShowRSVP(false)}
@@ -14,24 +21,24 @@ export const RSVPModal = ({ eventId }) => {
         <p>Fill in your personal information.</p>
         <label>
           Name:
-          <input />
+          <input required />
         </label>
         <label>
           Email:
-          <input type="email" />
+          <input type="email" required />
         </label>
         {!findEvent?.isPaid && (
           <p>* You have to make the payment at the venue</p>
         )}
         <button
-          onClick={() => {
-            setShowRSVP(false);
-            dispatch({ type: "RSVP", payload: eventId });
-          }}
+        // onClick={() => {
+        //   setShowRSVP(false);
+        //   dispatch({ type: "RSVP", payload: eventId });
+        // }}
         >
           RSVP
         </button>
-      </div>
+      </form>
     </div>
   );
 };
